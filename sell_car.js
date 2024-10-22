a402a891d63d7fc5599fd4c9032a5a45f932082d
Pham Duy Nghia <nghia1909200555@gmail.com>
2024-10-22 16:17:46 +0700

    update car selling page

--- sell_car.js
+++ sell_car.js
@@ -1,65 +1,90 @@
- function checkNull(txt){
+function checkNull(txt) {
     if (txt.value.length == 0)
         return true;
     return false;
- }
- function validForm(f,event){
+}
+const phoneRegex = /^[0-9]{3,}$/;
+const kmRegex = /^[0-9]{1,}$/;
+// ham check bieu thuc chinh qui
+function checkMatch(txt, regex) {
+    return regex.test(txt.value);
+}
+function validForm(f, event) {
     event.preventDefault();
     //  check rong brand
-    if (checkNull(f.carBrand)){
-        document.getElementById("brandStatus").style.display="block";
+    if (checkNull(f.carBrand)) {
+        document.getElementById("brandStatus").style.display = "block";
         f.carBrand.focus();
         return;
     }
-    else{
-        document.getElementById("brandStatus").style.display="none";
+    else {
+        document.getElementById("brandStatus").style.display = "none";
     }
-     //  check rong model
-    if (checkNull(f.carModel)){
-        document.getElementById("modelStatus").style.display="block";
+    //  check rong model
+    if (checkNull(f.carModel)) {
+        document.getElementById("modelStatus").style.display = "block";
         f.carModel.focus();
         return;
     }
-    else{
-        document.getElementById("modelStatus").style.display="none";
+    else {
+        document.getElementById("modelStatus").style.display = "none";
     }
-     //  check rong year
-    if (checkNull(f.yearOfManufacture)){
-        document.getElementById("yearStatus").style.display="block";
+    //  check rong year
+    if (checkNull(f.yearOfManufacture)) {
+        document.getElementById("yearStatus").style.display = "block";
         f.yearOfManufacture.focus();
         return;
     }
-    else{
-        document.getElementById("yearStatus").style.display="none";
+    else {
+        document.getElementById("yearStatus").style.display = "none";
     }
     //  check rong phien ban
-    if (checkNull(f.version)){
-        document.getElementById("versionStatus").style.display="block";
+    if (checkNull(f.version)) {
+        document.getElementById("versionStatus").style.display = "block";
         f.version.focus();
         return;
     }
-    else{
-        document.getElementById("versionStatus").style.display="none";
+    else {
+        document.getElementById("versionStatus").style.display = "none";
     }
     //  check rong so km da di
-    if (checkNull(f.mileage)){
-        document.getElementById("mileageStatus").style.display="block";
+    if (checkNull(f.mileage)) {
+        document.getElementById("mileageStatus").style.display = "block";
         f.mileage.focus();
         return;
     }
-    else{
-        document.getElementById("mileageStatus").style.display="none";
+    else {
+        document.getElementById("mileageStatus").style.display = "none";
     }
-     //  check rong so dien thoai
-     if (checkNull(f.phoneNumber)){
-        document.getElementById("phoneNumberStatus").style.display="block";
+    //  check rong so dien thoai
+    if (checkNull(f.phoneNumber)) {
+        document.getElementById("phoneNumberStatus").style.display = "block";
+        f.phoneNumber.focus();
+        return;
+    }
+    else {
+        document.getElementById("phoneNumberStatus").style.display = "none";
+    }
+    // check dinh dang so dien thoai
+    if(!checkMatch(f.phoneNumber, phoneRegex)){
+        document.getElementById("phoneNumberStatus").style.display = "block";
+        document.getElementById("phoneNumberStatus").innerHTML = "*Định dạng số điện thoại không hợp lệ";
         f.phoneNumber.focus();
         return;
     }
     else{
-        document.getElementById("phoneNumberStatus").style.display="none";
+        document.getElementById("phoneNumberStatus").style.display = "none";
     }
-    
+    // check dinh dang so km
+    if(!checkMatch(f.mileage , kmRegex)){
+        document.getElementById("mileageStatus").style.display = "block";
+        document.getElementById("mileageStatus").innerHTML = "*số km không hợp lệ";
+        f.phoneNumber.focus();
+        return;
+    }
+    else{
+        document.getElementById("mileageStatus").style.display = "none";
+    }
     alert("Bạn đã nhập thành công!");
     f.submit(); // Chỉ gửi form nếu tất cả đều hợp lệ
- }
+}
